529ec1b1c1a76a2ac6c85f5c6d5c180280e139dd
Dave Wallace <dwallacelf@gmail.com>
2017-09-26 22:07:51 -0400

    Fix name of VCL LD_PRELOAD lib dir env var.
    Change-Id: I86a3ffc6c119eb9979580e8d48979dce5d5ce72d
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- vcl-ldpreload/env.sh
+++ vcl-ldpreload/env.sh
@@ -2,4 +2,4 @@ export VCL_BASE=/home/alagalah/git/work/fdio
 export VPP_DIR=$VCL_BASE/vpp
 export LDP_DIR=$VCL_BASE/vppsb
 export WS_ROOT=$VCL_BASE/vpp
-export VCL_LDPRELOAD_LIB=$VCL_BASE/vppsb/vcl-ldpreload/src/.libs
+export VCL_LDPRELOAD_LIB_DIR=$VCL_BASE/vppsb/vcl-ldpreload/src/.libs
